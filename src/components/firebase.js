67dc9d622b6be7e5af12dd83e719a5a14fd1bb37
Pabitra Kumar Ghorai <pkg2000696@gmail.com>
2025-08-10 14:55:33 +0530

    add all the files

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import Firestore if needed
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth();
 export const db = getFirestore(app); // Assuming you want to use Firestore as well
